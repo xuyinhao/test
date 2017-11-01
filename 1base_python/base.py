e36e31d0dff4f3e3f3f57e63d854aac663923099
xuyinhao <yinhaoxu@hotmail.com>
2017-11-01 21:27:10 +0800

    1basepy-2017111-2126

---
+++ 1base_python/base.py
@@ -0,0 +1,39 @@
+
+#-*- coding:utf-8 -*-
+#浮点 1.0 / 2.0
+#整除 1 // 2
+#取余数（模除） 1 % 2
+#长整数 102000000000000000000000L
+#十六进制、八进制 0xAF   071
+#变量 x=3 x*2
+#语句
+#获取用户输入   a=input("x:")
+        #raw_input
+
+#if 语句
+#if 1 == 2 : print '1'
+#if 2 == 2  :print '2'
+
+#函数
+# print pow(2,3)  #2的次幂
+#abs 绝对值   ；  round浮点数 四舍五入
+
+#模块
+import math
+# print int(math.floor(32.012))
+#cmath 复数 complex math
+# import cmath
+# print(cmath.sqrt(-1))
+
+#转义-
+# tmp=222
+# print("\"let's go\":%s"%tmp)
+# print("asa"+`tmp`)  #`` 字符串
+# print("asa"+repr(tmp))  #字符串转换成合法的py 表达式
+
+
+# name=raw_input("what's your name: ")
+# print("my name is : "+name+ " !")
+
+#长字符串
+
