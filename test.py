72ecc5b7185a44b0d8e9bb8981132cf1635107c3
yinhaoxu <yinhaoxu@hotmail.com>
2017-11-02 18:09:23 +0800

    1basepy-2017112-1808

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-from smtplib import SMTP
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+data=open('../user_passwd','r')
+data_line=data.readline()
+data.close()
+
+smtpserver=data_line.split(',')[0]
+user=data_line.split(',')[1]
+password=data_line.split(',')[2]
+recer=data_line.split(',')[3]
+
+subject='python email test'
+#html 邮件的正文
+msg=MIMEText('<html><h1>傻孩子 你好</h1></html>','html','utf-8')
+msg['Subject'] = Header(subject,'utf-8')
+
+#连接发送邮件
+smtp=smtplib.SMTP()
+smtp.connect(smtpserver,25)
+smtp.login(user,password)
+smtp.sendmail(user,recer,msg.as_string())
+smtp.quit()
