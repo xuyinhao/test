d465c67eeafba59bdec804fce2bef21d44b9908e
xuyinhao <yinhaoxu@hotmail.com>
2017-11-08 21:42:43 +0800

    6def_1108

--- 6def_/storage_userinfo.py
+++ 6def_/storage_userinfo.py
@@ -7,22 +7,24 @@ def init(data):
     data['last']={}
 
 def lookup(data,label,name):
-    return data[label].get(name)       #字典，得到含有这个name key的所有value
+    return data[label].get(name)
+    #字典，得到含有这个name key的所有value
 
-def store(data,fullname):
-    names=fullname.split()
-    labels=['first','second','last']
-    if len(names) == 2 : lables['second']=' '
-    for label,name in zip(labels,names):
-        people=lookup(data,label,name)
-        if people:
-            people.append(fullname)
-        else:
-            data[label][name]=[fullname]
+def store(data,*fullname):
+    for fname in fullname:
+        names=fname.split()
+        labels=['first','second','last']
+        if len(names) == 2 : names.insert(1,'')
+        for label,name in zip(labels,names):
+            people=lookup(data,label,name)
+            if people:
+                people.append(fname)
+            else:
+                data[label][name]=[fname]
 storage={}
 init(storage)
 # store(storage,"徐 银 号")
-store(storage,"徐 银 杰")
+store(storage,"徐 银 杰","蒋 猪","蒋 狗")
 store(storage,"徐 一 号")
 store(storage,"徐 一 号")
 # store(storage,"二 一 号")
